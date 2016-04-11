7d0d4680edc667cec1de807f1a9fab60f08d25a7
Tom Claasens <thomas-claasens@outlook.com>
2016-04-11 08:41:54 +0200

    Update

--- GuildMemberDetails/Form1.cs
+++ GuildMemberDetails/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BattleDotNet;
 
 namespace GuildMemberDetails
 {
     public partial class Form1 : Form
     {
+        private string apiKey = "qrfwx6c29zs8g2hw8ze2qz7yke9r36v2";
+        private string secret = "Gx7usVwT3vfKnE3GuWsq5y2hjGYYNjjZ";
+        private BattleDotNet.WoWClient bNetClient;
         public Form1()
         {
             InitializeComponent();
+            bNetClient = new WoWClient(BattleDotNet.Region.EU, apiKey, secret);
+            Console.WriteLine(bNetClient.GetAuctions("silvermoon").Alliance.Count());
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var guild = bNetClient.GetGuild("Forward únto Dawn", "Silvermoon");
+            Console.WriteLine(guild.Members.Count());
+
         }
     }
 }
